abb4d814b9fc11bc492553c68ed0c63c5840eb79
escching <96568344+escching@users.noreply.github.com>
2022-01-03 00:42:13 +0800

    Add files via upload

--- Folder1/start.py
+++ Folder1/start.py
@@ -1,1 +1,16 @@
+import numpy as np
+import SpikingNeuronModel as m
 
+np.random.seed(0)
+loadFromPrev = False
+totTime = 7500
+timeStep = 0.125
+plotStep = round(500/timeStep)
+network = 'networkC.txt'
+
+myModel = m.SpikingNeuronModel(loadFromPrev=loadFromPrev)
+myModel.initNetwork(network)
+myModel.initDynamicalParams()
+myModel.initDynamics(totIter=round(totTime/timeStep), timeStep=timeStep, plotStep=plotStep)
+myModel.runDynamics()
+myModel.saveDynamicsAndPlot()
